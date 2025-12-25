39c159fbc42448dfb6f09bc58e67018673b868e2
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-12-25 15:08:42 +0530

    array

---
+++ Arrays/three.js
@@ -0,0 +1,3 @@
+let arr=[15,45,78,96,85];
+
+console.log(arr.fill(5));
---
+++ String/Stronr.js
@@ -0,0 +1,9 @@
+//String Literal
+let str = new String('hello world');
+console.log(str);
+
+//String Constructor
+
+let str1 = String('hello world 2');
+console.log(str);
+
