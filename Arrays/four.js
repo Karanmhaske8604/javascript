d25421780036648e71021bdfb0982f545560f808
Karanmhaske8604 <karanmhaske3050@gamil.com>
2025-12-25 15:20:30 +0530

    Array

---
+++ Arrays/four.js
@@ -0,0 +1,5 @@
+let arr=[2,4,6,8,9];
+arr2=arr;
+arr2=[];
+
+console.log(arr2);
--- String/Stronr.js
+++
@@ -1,9 +0,0 @@
-//String Literal
-let str = new String('hello world');
-console.log(str);
-
-//String Constructor
-
-let str1 = String('hello world 2');
-console.log(str);
-
