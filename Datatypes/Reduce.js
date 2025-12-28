0041086abd413236ff94652a71ff8a54e18b0eb7
Karanmhaske8604 <karanmhaske3050@gmail.com>
2025-12-28 21:56:56 +0530

    Methods

---
+++ Datatypes/Reduce.js
@@ -0,0 +1,11 @@
+let arr=[3,4,12,13];
+console.log(arr.reduce(minus));
+
+//Reduce => APPLIES CALLBACK FUNCTION AS IT IS IN THE SAME ORDER.
+//reduceRight()=> Applies the callback function in the descenfing order.
+//10-20=>-10-30=>-40-40=>-80
+//2*3*4=>24
+function minus(i,j)
+{
+    return i*j;
+}
