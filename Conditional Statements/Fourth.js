4172ee2d26797a5e744b83f4f0da815e984966b1
Karanmhaske8604 <karanmhaske3050@gmail.com>
2026-01-17 23:48:55 +0530

    IFELSE LADDER

---
+++ Conditional Statements/Fourth.js
@@ -0,0 +1,17 @@
+
+let age=200;
+if(age >=18 && age<=30)  //18-30
+{
+    console.log("Vote GraceFully !");
+    
+}
+else if(age>=31 && age <=50)  //31-50
+{
+    console.log("Vote Carefully !");
+    
+}
+//0-17 && 51-infinite
+else{
+    console.log("Unable to vote..");
+    
+}
--- Conditional Statements/third.js
+++ Conditional Statements/third.js
@@ -2,10 +2,10 @@
 let age=32;
 if(age>50 && age <60)
 {
-    console.log("eneligble");
+    console.log("eneligible");
     
 }
 else{
-    console.log("elgible");
+    console.log("eligible");
     
 }
