808e8f237c834ad247fcd673e09feda2b6a25d10
Karanmhaske8604 <karanmhaske3050@gmail.com>
2026-05-27 11:40:51 +0530

    setTimeOut

--- Async_Prog/One.js
+++ Async_Prog/One.js
@@ -1,15 +1,16 @@
 
-console.log("hii");
-function Demo()
+console.log("Step 1 to execute...");
 
+setTimeout(demo,3000);
+
+function demo()
 {
-    console.log("hello");
-    
+    console.log("I am from function demo......");
+    console.log("Step 2 to be executed..");
 }
 
-console.log("welcome");
+console.log("Step 3 to execute....");
 
 
-Demo();
 
-console.log("kdkm");
+
